21631f2b41023cf8b871444698d8de5de31c61d5
Raghav <bathla.raghav5@gmail.com>
2022-11-15 09:14:01 +0530

    [Raghav] Test message

---
+++ src/com/company/Main.java
@@ -0,0 +1,30 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int num = 0;
+        Scanner Scan = new Scanner(System.in);
+        System.out.println("Enter Number");
+        num= Scan.nextInt();
+
+// 30
+
+        boolean flag = false;
+
+
+        for (int i = 2; i < num /2 ; ++i) {
+            if(num % i == 0 ) {
+                flag =true;
+                break;
+            }
+        }
+        if(!flag)
+            System.out.println(num + " is a prime number. ");
+        else
+            System.out.println(num + " is Not Prime Number. ");
+    }
+    }
+
